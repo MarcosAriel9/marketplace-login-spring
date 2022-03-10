577c8de8a08e2c5d5bbb136abfeed3645b3f10d2
MikeMoreno <netmovez_01@hotmail.com>
2022-03-10 14:54:09 -0600

    [UPDATE] Security

--- src/main/java/mx/edu/utez/marketplace/security/MainSecurity.java
+++ src/main/java/mx/edu/utez/marketplace/security/MainSecurity.java
@@ -1,4 +1,42 @@
 package mx.edu.utez.marketplace.security;
 
-public class MainSecurity {
+import mx.edu.utez.marketplace.security.controller.AuthService;
+import mx.edu.utez.marketplace.security.jwt.JwtEntryPoint;
+import mx.edu.utez.marketplace.security.jwt.JwtTokenFilter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class MainSecurity extends WebSecurityConfigurerAdapter {
+    @Autowired
+    AuthService authService;
+    @Autowired
+    JwtEntryPoint entryPoint;
+
+    @Bean
+    public JwtTokenFilter jwtTokenFilter(){
+        return new JwtTokenFilter();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception{
+        auth.userDetailsService(authService).passwordEncoder(passwordEncoder());
+    }
+
+
+
 }
